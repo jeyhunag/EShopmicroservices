8c061a90c3568d234116b9a369a6df6168ca4090
jeyhunag <104305445+jeyhunag@users.noreply.github.com>
2024-12-19 14:44:02 +0400

    Create GetProducts

---
+++ src/Services/Catalog/CatalogAPI/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,24 @@
+﻿namespace CatalogAPI.Products.GetProducts;
+
+//public record GetProductsRequest();
+
+public record GetProductsResponse(IEnumerable<Product>Products);
+public class GetProductsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/Products", async (ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductsQuery());
+
+            var response=result.Adapt<GetProductsResponse>();
+
+            return Results.Ok(response);
+        })
+         .WithName("GetProducts")
+        .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Products")
+        .WithDescription("Get Products");
+    }
+}
